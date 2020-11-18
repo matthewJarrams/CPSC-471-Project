c033be533d966a4d7602cde4332d0a870a402d3b
matthewJarrams <matthew.jarrams@gmail.com>
2020-11-17 18:05:21 -0700

    Create search-reviews.php

---
+++ search-reviews.php
@@ -0,0 +1,66 @@
+<!-- Adapted from Coding with Elias https://www.youtube.com/watch?v=JDn6OAMnJwQ and https://www.youtube.com/watch?v=QxZxHUf7c_0 -->
+<?php
+session_start();
+include "db_conn.php";
+
+
+if (isset($_SESSION['ID']) && isset($_SESSION['Username'])) {
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SEARCH REVIEWS</title>
+    <link rel = "stylesheet" type = "text/css" href = "style.css">
+</head>
+
+<body>
+
+<h1>
+<?php
+    // Get all user info from database and print it out
+    $sql = "SELECT * FROM class_review";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    // There does exist users in the database
+    if ($resultCheck > 0) {
+        
+
+        // Print Header
+        echo  'Class Code' . "  |  " . 'Would take again' . "  |  " . 'Required' . "  |  " .
+                  'Textbook' . "  |  " . 'Work load' . "  |  " . 'Difficulty' . "  |  " .
+                  'Semester' . "  |  " . 'Year' ."<br>";
+
+        // Print out each entry in given course code
+        while($row = mysqli_fetch_assoc($result)) {
+            echo  $row['Class_code'] . "  |  " . $row['Would_take_again'] . "  |  " . $row['Required'] . "  |  " .
+                  $row['Textbook'] . "  |  " . $row['Work_load'] . "  |  " . $row['Difficulty'] . "  |  " .
+                  $row['Semester'] . "  |  " . $row['Year'] ."<br>";
+            
+        }
+    }
+?>
+</h1>
+   <!-- <h1>Hello, <?php echo $_SESSION['Username']; ?></h1><br> -->    
+
+
+    <a href="logout.php" class = "logoutLblPos">Logout</a>
+
+
+</body>
+
+
+
+</html>
+
+<?php
+}else{
+    header("Location: index.php");
+    exit();
+
+}
+?>
