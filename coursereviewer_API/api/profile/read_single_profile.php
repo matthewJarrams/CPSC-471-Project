f0c89cf85bb87ae8c84e5e37e8316412eed1ecbc
matthewJarrams <matthew.jarrams@gmail.com>
2020-12-08 18:58:32 -0700

    Fixed minors in bug

--- coursereviewer_API/api/profile/read_single_profile.php
+++ coursereviewer_API/api/profile/read_single_profile.php
@@ -19,15 +19,20 @@ $post->read_single();
 //test variable to see if undergrad or graduate
 $check = $post->Year;
 
+$result2 = $post->get_minor();
+$row2 = $result2->fetch(PDO::FETCH_ASSOC);
+$MinCode = $row2['MinD_code'];
+
 //if undergrad (they have a year)
 if($check != null)
 {
-	$post_arr = array('ID' => $post->ID,'First_name' => $post->First_name,'Last_name' => $post->Last_name,'Date_made' => $post->Date_made,'Username' => $post->Username, 'email_address' => $post->email_address, 'University' => $post->University,	'Dep_code' => $post->Dep_code,	'Has_graduated' => $post->UGHas_graduated,	'Year' => $post->Year,	'Concentration' => $post->Concentration,	'Faculty' => $post->UGFaculty, 'MinD_code' => $post->MinD_code);
+	$post_arr = array('ID' => $post->ID,'First_name' => $post->First_name,'Last_name' => $post->Last_name,'Date_made' => $post->Date_made,'Username' => $post->Username, 'email_address' => $post->email_address, 'University' => $post->University,	'Dep_code' => $post->Dep_code,	'Has_graduated' => $post->UGHas_graduated,	'Year' => $post->Year,	'Concentration' => $post->Concentration,'Faculty' => $post->UGFaculty, 'Minor Department' => $MinCode);
+	
 }
 //else student is a graduate
 else
 {
-	$post_arr = array('ID' => $post->ID,'First_name' => $post->First_name,'Last_name' => $post->Last_name,'Date_made' => $post->Date_made,'Username' => $post->Username, 'email_address' => $post->email_address, 'University' => $post->University,	'Dep_code' => $post->GDep_code,	'Has_graduated' => $post->GHas_graduated,	'Research_interest' => $post->Research_interest,	'Faculty' => $post->GFaculty, 'MinD_code' => $post->MinD_code);
+	$post_arr = array('ID' => $post->ID,'First_name' => $post->First_name,'Last_name' => $post->Last_name,'Date_made' => $post->Date_made,'Username' => $post->Username, 'email_address' => $post->email_address, 'University' => $post->University,	'Dep_code' => $post->GDep_code,	'Has_graduated' => $post->GHas_graduated,	'Research_interest' => $post->Research_interest,	'Faculty' => $post->GFaculty, 'Minor Department' => $MinCode);
 
 }
 
